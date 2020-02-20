aabce35307f1ab7b7921ae273fe7d6a16e579c88
stebeLski <antonstebelski@gmail.com>
2020-02-20 11:33:39 +0200

    set up express server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`server up on http://localhost:${PORT}/`);
+});
