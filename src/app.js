702ce57a428ef4a0ccaed16c6dd6bb54f18ba030
stebeLski <antonstebelski@gmail.com>
2020-02-20 16:50:39 +0200

    add passports strategies configs and start implement login route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-require('dotenv').config();
-
 const express = require('express');
+const passport = require('passport');
+
+require('dotenv').config();
+require('./config/passport')(passport);
+
+const authRoute = require('./users/route');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRoute);
 
 app.listen(PORT, () => {
   console.log(`server up on http://localhost:${PORT}/`);
---
+++ src/config/passport.js
@@ -0,0 +1,43 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const db = require('../db/index');
+
+module.exports = (passport) => {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: 'name', passwordField: 'password' },
+      (username, password, done) => {
+        const sql = `SELECT id, name, password
+                   FROM users
+                   WHERE name = ?`;
+        db.get(sql, [username], (err, row) => {
+          if (!row) {
+            return done(null, false, {
+              message: 'There is no user with this name',
+            });
+          }
+          bcrypt.compare(password, row.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+              const { password: removePass, ...rowdata } = row;
+              return done(null, rowdata);
+            }
+            console.log('pass inncorect');
+            return done(null, false, {
+              message: 'Password incorrect',
+            });
+          });
+        });
+      },
+    ),
+  );
+
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  passport.deserializeUser((id, done) => {
+    db.get('SELECT id, name FROM users WHERE id = ?', [id], (err, row) => {
+      if (!row) return done(null, false);
+      return done(null, row);
+    });
+  });
+};
